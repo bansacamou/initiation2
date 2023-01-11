6fdcb8d6e59acbaae1094c7b821d757b4f100249
Moussa CAMARA <guedel13@gmail.com>
2023-01-11 18:46:38 +0000

    Mis à jour des fichiers

---
+++ fichier.py
@@ -0,0 +1,3 @@
+class Git:
+	def depotlocal():
+		return "ce depot de git permet de travail en local sans connexion internet"
---
+++ fichier2.py
@@ -0,0 +1,3 @@
+class GitHub():
+	def depotdistant():
+		return "ce depot de git permet de travail à distance avec connexion internet"
